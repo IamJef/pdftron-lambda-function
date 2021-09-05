0da9fd03e7d2b06446f69bd3906465083d542b23
Jeferson Gomes <jeffoliveiranal@gmail.com>
2021-09-05 19:55:15 -0300

    feat: encrypt pdf file when uploaded in lms bucket

---
+++ README.md
@@ -0,0 +1,21 @@
+# at-pdfenc
+
+## Prerequisites
+
+* Node 12.x
+* NPM 7.4.x
+* Serverless CLI [Serverless Framework](Serverless.com.br)
+
+## Config
+Edit **serverless.yml** and set your configs. Pdftron License Key, Bucket,
+encrypt key and trigger to call function.
+
+## How it works
+
+* User upload **PDF** file in LMS bucket (defined in the serverless.yml file)
+* Function get this file and encrypt with Key defined
+* Save new file in **encrypted** folder inside the bucket
+
+## Working Flow
+![Working Flow](https://i.imgur.com/l64WfCQ.png)
+
--- encrypt.js
+++ encrypt.js
@@ -1,48 +1,47 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-const S3 = new AWS.S3();
+const s3 = new AWS.S3();
 const { PDFNet } = require('@pdftron/pdfnet-node');
 const { basename, extname } = require('path');
 
-module.exports.handle = async ({Records: records}, context) => {
+module.exports.handle = async (event, context) => {
+  let response = null;
   const main = async () => {
-      let response = null;
-      await Promisse.all(records.map(async record => {
-        const { key } = decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '));
-  
-        const file = await S3.getObject({
-          Bucket: process.env.bucket,
-          Key: key,
-        }).promise();
-  
-        const arr = new Uint8Array(file.body);
-        const doc = await PDFNet.PDFDoc.createFromLayoutEls(arr);
-        const newHandler = await PDFNet.SecurityHandler.createDefault(); 
-        
-        newHandler.changeUserPasswordUString(process.env.hash);
-        newHandler.setPermission(PDFNet.SecurityHandler.Permission.e_print, false);
-        await newHandler.setPermission(PDFNet.SecurityHandler.Permission.e_extract_content, false);
-  
-        doc.setSecurityHandler(newHandler);
-        const encrypted = doc.saveMemoryBuffer();
-  
-        await S3.putObject({
-          Body: encrypted,
-          Bucket: process.env.bucket,
-          ContentType: 'application/pdf',
-          Key: `encrypted/${basename(key, extname(key))}.pdf`
-        }).promise()
-        response = {
-          statusCode: 301,
-          body: JSON.stringify("Encrypt with success")
-        }
-      }));
+      const key  = event.Records[0].s3.object.key;
+
+      const file = await s3.getObject({
+        Bucket: process.env.bucket,
+        Key: key,
+      }).promise();
+      
+      const arr = new Uint8Array(file.Body);
+      const doc = await PDFNet.PDFDoc.createFromBuffer(arr);
+      const newHandler = await PDFNet.SecurityHandler.createDefault(); 
+    
+
+      newHandler.changeUserPasswordUString(process.env.hash);
+      newHandler.setPermission(PDFNet.SecurityHandler.Permission.e_print, false);
+      newHandler.setPermission(PDFNet.SecurityHandler.Permission.e_extract_content, false);
+
+      doc.setSecurityHandler(newHandler);
+      const encrypted = await doc.saveMemoryBuffer(arr.length);
+      var base64data = new Buffer.from(encrypted, 'binary');
+
+      await s3.putObject({
+        Body: base64data,
+        Bucket: process.env.bucket,
+        ContentType: 'application/pdf',
+        Key: `encrypted/${basename(key, extname(key))}.pdf`
+      }).promise()
+    
+    response = {
+      statusCode: 301,
+      body: JSON.stringify("File encrypted")
+    }
   }
   
   await PDFNet.runWithCleanup(main, process.env.pdftronkey).catch(function (error) {
-    console.log('Error: ' + JSON.stringify(error));
-
       response = {
         statusCode: 500,
         body: JSON.stringify(error),
